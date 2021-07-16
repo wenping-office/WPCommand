4dc0ebc487c0560bf7f94aa8f9cb34191bb1cbf9
Developer <850500722@qq.com>
2021-07-16 18:29:44 +0800

    target 初次提交

---
+++ Example/Pods/Target Support Files/WPCommand/WPCommand-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WPCommand : NSObject
+@end
+@implementation PodsDummy_WPCommand
+@end
---
+++ Example/WPCommand/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  WPCommand
+//
+//  Created by Developer on 07/16/2021.
+//  Copyright (c) 2021 Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
