b3615d8a7ecbadeca4b57e4bb66e27d6d48a4196
Developer <850500722@qq.com>
2021-07-27 11:45:58 +0800

    target 调整代码

--- WPCommand/Classes/Public/Extension/Fundation/Date+Ex.swift
+++ WPCommand/Classes/Public/Extension/Fundation/Date+Ex.swift
@@ -121,66 +121,66 @@ public extension Date{
     /// 偏移年
     /// - Parameter year: 年
     /// - Returns: 结果
-    func wp_offSetYear(_ year:Int)->Date{
+    func wp_offSetYear(_ year:Int)->Date?{
         let date = self
         let calendar = Calendar.current
         var comps = calendar.dateComponents([.year], from: date)
         comps.year = year
-        return calendar.date(byAdding: comps, to: date)!
+        return calendar.date(byAdding: comps, to: date)
     }
     
     /// 偏移月
     /// - Parameter month: 月
     /// - Returns: 结果
-    func wp_offSetMonth(_ month:Int)->Date{
+    func wp_offSetMonth(_ month:Int)->Date?{
         let date = self
         let calendar = Calendar.current
         var comps = calendar.dateComponents([.month], from: date)
         comps.year = month
-        return calendar.date(byAdding: comps, to: date)!
+        return calendar.date(byAdding: comps, to: date)
     }
     
     /// 偏移日
     /// - Parameter day: 日
     /// - Returns: 结果
-    func wp_offSetDay(_ day:Int)->Date{
+    func wp_offSetDay(_ day:Int)->Date?{
         let date = self
         let calendar = Calendar.current
         var comps = calendar.dateComponents([.day], from: date)
         comps.year = day
-        return calendar.date(byAdding: comps, to: date)!
+        return calendar.date(byAdding: comps, to: date)
     }
     
     /// 偏移时
     /// - Parameter hour: 时
     /// - Returns: 结果
-    func wp_offSetHour(_ hour:Int)->Date{
+    func wp_offSetHour(_ hour:Int)->Date?{
         let date = self
         let calendar = Calendar.current
         var comps = calendar.dateComponents([.hour], from: date)
         comps.year = hour
-        return calendar.date(byAdding: comps, to: date)!
+        return calendar.date(byAdding: comps, to: date)
     }
     
     /// 偏移分
     /// - Parameter minute: 分
     /// - Returns: 结果
-    func wp_offSetMinute(_ minute:Int)->Date{
+    func wp_offSetMinute(_ minute:Int)->Date?{
         let date = self
         let calendar = Calendar.current
         var comps = calendar.dateComponents([.minute], from: date)
         comps.year = minute
-        return calendar.date(byAdding: comps, to: date)!
+        return calendar.date(byAdding: comps, to: date)
     }
     
     /// 偏移秒
     /// - Parameter second: 秒
     /// - Returns: 结果
-    func wp_offSetSecond(_ second:Int)->Date{
+    func wp_offSetSecond(_ second:Int)->Date?{
         let date = self
         let calendar = Calendar.current
         var comps = calendar.dateComponents([.second], from: date)
         comps.year = second
-        return calendar.date(byAdding: comps, to: date)!
+        return calendar.date(byAdding: comps, to: date)
     }
 }
