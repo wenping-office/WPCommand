4ee31271ed7eda390d2037f6a16b508333bc497a
文平 <850500722@qq.com>
2021-10-22 10:24:32 +0800

    target 新增高亮蒙层协议

--- WPCommand/Classes/Public/Protocol/WPHighlightMaskProtocol.swift
+++ WPCommand/Classes/Public/Protocol/WPHighlightMaskProtocol.swift
@@ -7,54 +7,46 @@
 
 import UIKit
 
-/// 高亮视图协议 实现协议后会持有高亮方法
-public protocol WPHighlightViewProtocol:UIView {
-    /// 蒙层被点击了
-    func highlighMaskTouch(tapGesture:UITapGestureRecognizer)
-    /// 设置高亮时的颜色
-    func highlighMaskDidSet(color:UIColor)
+/// 高亮蒙层协议、实现协议后拥有高亮显示方法
+public protocol WPHighlightMaskProtocol:UIView{
+    /// 高亮顶部视图
+    func highlightTopView() -> WPHighlightViewProtocol
+    /// 高亮底部视图
+    func highlightBottomView() -> WPHighlightViewProtocol
+    /// 高亮左边视图
+    func highlightLeftView() -> WPHighlightViewProtocol
+    /// 高亮右边视图
+    func highlightRightView() -> WPHighlightViewProtocol
+    /// 高亮中心视图
+    func highlightCenterView()->WPHighlightViewProtocol
 }
 
-public extension WPHighlightViewProtocol{
-    /// 蒙层被点击了
-    func highlighMaskTouch(tapGesture:UITapGestureRecognizer){}
-    /// 设置高亮时的颜色
-    func highlighMaskDidSet(color:UIColor){}
-}
-
-/// 高亮蒙层视图内部使用
-class WPHighlighMaskView:UIView,WPHighlightViewProtocol {}
-
-public extension UIView{
-    
+public extension WPHighlightMaskProtocol{
     /// 高亮顶部视图
     func highlightTopView() -> WPHighlightViewProtocol {
         return WPHighlighMaskView()
     }
-
     /// 高亮底部视图
     func highlightBottomView() -> WPHighlightViewProtocol {
         return WPHighlighMaskView()
     }
-    
     /// 高亮左边视图
     func highlightLeftView() -> WPHighlightViewProtocol {
         return WPHighlighMaskView()
     }
-    
     /// 高亮右边视图
     func highlightRightView() -> WPHighlightViewProtocol {
         return WPHighlighMaskView()
     }
-    
     /// 高亮中心视图
     func highlightCenterView()->WPHighlightViewProtocol{
         return WPHighlighMaskView()
     }
-
+    
     /// 显示高亮到某个视图
     /// - Parameter view: 高亮添加到的视图 注：view的frame必须稳定以后才可以调用、否则有可能显示位置不准,并且view必须是self的底层，看起来要比view一般是widow或者view的superView
     func showHighlight(to view:UIView? = nil,
+                       touch:((WPHighlightViewProtocol)->Void)? = nil,
                        color : UIColor = UIColor.init(0, 0, 0, 0.4)){
         
         removeHighlight(of: view)
@@ -80,15 +72,19 @@ public extension UIView{
         centerView.addGestureRecognizer(centerTapGesture)
         topTapGesture.rx.event.bind(onNext: { gesture in
             topView.highlighMaskTouch(tapGesture: gesture)
+            touch?(topView)
         }).disposed(by: topView.wp_disposeBag)
         bottomTapGesture.rx.event.bind(onNext: { gesture in
             bottomView.highlighMaskTouch(tapGesture: gesture)
+            touch?(bottomView)
         }).disposed(by: topView.wp_disposeBag)
         leftTapGesture.rx.event.bind(onNext: { gesture in
             leftView.highlighMaskTouch(tapGesture: gesture)
+            touch?(leftView)
         }).disposed(by: topView.wp_disposeBag)
         rightTapGesture.rx.event.bind(onNext: { gesture in
             rightView.highlighMaskTouch(tapGesture: gesture)
+            touch?(rightView)
         }).disposed(by: topView.wp_disposeBag)
 
         topView.backgroundColor = color
@@ -141,7 +137,8 @@ public extension UIView{
     }
     
     /// 从某个视图移除高亮视图
-    func removeHighlight(of view:UIView? = nil){
+    func removeHighlight(of view:UIView? = nil,
+                                   completion:((WPHighlightMaskProtocol)->Void)? = nil){
         let keyView = view != nil ? view : UIApplication.shared.wp_topWindow
         keyView?.subviews.forEach({ elmt in
             let highView = elmt as? WPHighlightViewProtocol
@@ -152,7 +149,29 @@ public extension UIView{
             let highView = elmt as? WPHighlightViewProtocol
             highView?.removeFromSuperview()
         })
+
+        completion?(self)
     }
 }
+
+/// 高亮视图协议 实现协议后会返回高亮时创建的视图
+public protocol WPHighlightViewProtocol:UIView {
+    /// 蒙层被点击了
+    func highlighMaskTouch(tapGesture:UITapGestureRecognizer)
+    /// 设置高亮时的颜色
+    func highlighMaskDidSet(color:UIColor)
+}
+
+public extension WPHighlightViewProtocol{
+    /// 蒙层被点击了
+    func highlighMaskTouch(tapGesture:UITapGestureRecognizer){}
+    /// 设置高亮时的颜色
+    func highlighMaskDidSet(color:UIColor){}
+}
+
+/// 高亮蒙层视图内部使用
+class WPHighlighMaskView:UIView,WPHighlightViewProtocol {}
+
+extension UIView:WPHighlightMaskProtocol{}
 
 
