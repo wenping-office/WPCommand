afeacc9cc1e25fadb74a627c4553f9f061d6d6c6
Tate <188083699@qq.com>
2026-04-09 16:33:05 +0800

    调整代码

--- Sources/WPCommand/Extension/RxSwift/Combine+Rx.swift
+++ Sources/WPCommand/Extension/RxSwift/Combine+Rx.swift
@@ -37,3 +37,24 @@ public extension ObservableType {
             .eraseToAnyPublisher()
     }
 }
+
+
+public extension Publisher {
+    /// 闭包形式通知序列执行，源序列成功后才会执行
+    /// 最终只输出源序列的值，不输出内部序列的值
+    /// - Parameter source: 返回新序列的闭包
+    /// - Returns: 只输出源序列值的 Publisher
+    func notice<T>(_ source: @escaping (Output) -> AnyPublisher<T, Never>) -> AnyPublisher<Output, Failure> {
+        return self
+            .flatMap { value -> AnyPublisher<Output, Failure> in
+                // 执行通知序列，然后只输出源值
+                source(value)
+                    .flatMap { _ in Empty<Output, Never>() }  // 忽略通知序列的所有值
+                    .catch { _ in Empty<Output, Never>() }    // 忽略错误
+                    .append(value)  // 追加源值
+                    .setFailureType(to: Failure.self)
+                    .eraseToAnyPublisher()
+            }
+            .eraseToAnyPublisher()
+    }
+}
